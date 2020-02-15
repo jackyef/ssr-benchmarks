b5b345c0679596bb7269aba604b4ca63bc6c323b
Jacky Efendi <jacky.efendi1@gmail.com>
2020-02-15 15:29:46 +0700

    feat: added react-esx-ssr

---
+++ methods/react-esx-ssr/babel.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  babelrc: false,
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        modules: "commonjs",
+        useBuiltIns: "entry",
+        corejs: "2",
+        exclude: ["es6.promise", "es7.promise.finally"]
+      }
+    ],
+    ["@babel/preset-react", { development: false, useBuiltIns: true }]
+  ],
+  plugins: [
+    "babel-plugin-macros",
+    ["@babel/plugin-proposal-decorators", { legacy: true }],
+    ["@babel/plugin-proposal-class-properties", { loose: true }],
+    "@babel/plugin-proposal-export-default-from",
+    "@babel/plugin-proposal-export-namespace-from",
+    ["@babel/plugin-proposal-object-rest-spread", { useBuiltIns: true }],
+    "@babel/plugin-proposal-optional-chaining",
+    "@babel/plugin-proposal-nullish-coalescing-operator",
+    "@babel/plugin-syntax-async-generators",
+    "@babel/plugin-syntax-dynamic-import",
+    ["@babel/plugin-transform-destructuring", { useBuiltIns: true }],
+    ["@babel/plugin-transform-runtime", { helpers: false, regenerator: true }],
+    "esx-ssr"
+  ].filter(Boolean)
+};
---
+++ methods/react-esx-ssr/index.jsx
@@ -0,0 +1,75 @@
+const React = require("react");
+const { renderToString } = require("react-dom/server");
+
+const RecursiveDivs = ({ depth = 1, breadth = 1 }) => {
+  if (depth <= 0) {
+    return <div>abcdefghij</div>;
+  }
+
+  let children = [];
+
+  for (let i = 0; i < breadth; i++) {
+    children.push(
+      <RecursiveDivs key={i} depth={depth - 1} breadth={breadth - 1} />
+    );
+  }
+
+  return (
+    <div
+      onClick={() => {
+        console.log("clicked");
+      }}
+    >
+      {children}
+    </div>
+  );
+};
+
+const warmUpV8 = () => {
+  console.info("Warming up...");
+
+  for (let i = 0; i < 20; i += 1) {
+    renderToString(<RecursiveDivs depth={5} breadth={11} />);
+  }
+
+  console.info("Finished warming up!");
+};
+
+const benchmark = () => {
+  let time = [];
+
+  for (let i = 0; i < 30; i += 1) {
+    const start = process.hrtime();
+
+    // this renders around 64472 divs
+    const markup = renderToString(<RecursiveDivs depth={5} breadth={11} />);
+    
+    time.push(process.hrtime(start));
+
+    require('fs').writeFileSync('./dist/test.html', markup);
+
+  }
+
+  console.info("================ RESULT ================");
+  const durations = time.map(t => (t[0] + t[1] / 1e9) * 1e3);
+
+  durations.forEach((d, i) => {
+    console.info(`Run ${i} took `, d, "ms");
+  });
+  
+  console.info("================ SUMMARY ================");
+  console.info("[react-esx-ssr]");
+  console.info(
+    "Average is:",
+    durations.reduce((a, b) => a + b) / durations.length,
+    "ms"
+  );
+  console.info(
+    "Stdev is:",
+    require('node-stdev').population(durations),
+    "ms"
+  );
+};
+
+warmUpV8();
+benchmark();

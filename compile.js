f2790fedf03cdf815d1ca884bf89a7030f1157b7
Jacky Efendi <jacky.efendi1@gmail.com>
2020-02-16 15:35:48 +0700

    improvement: sort result from fastest

--- compile.js
+++ compile.js
@@ -2,5 +2,5 @@ const fs = require('fs');
 const methods = fs.readdirSync('./methods');
 
 console.log('```');
-console.table(methods.map(method => require(`./methods/${method}/dist/result.json`)));
+console.table(methods.map(method => require(`./methods/${method}/dist/result.json`)).sort((a,b) => a.average - b.average));
 console.log('```');

8b996566ec1007a7b343e5d7969102e1172c7fb5
Jacky Efendi <jacky.efendi1@gmail.com>
2020-02-16 15:28:44 +0700

    chore: cleanup deps of preact methods

--- methods/preact-htm-ssr/babel.config.js
+++ methods/preact-htm-ssr/babel.config.js
@@ -10,19 +10,5 @@ module.exports = {
         exclude: ["es6.promise", "es7.promise.finally"]
       }
     ],
-  ],
-  plugins: [
-    "babel-plugin-macros",
-    ["@babel/plugin-proposal-decorators", { legacy: true }],
-    ["@babel/plugin-proposal-class-properties", { loose: true }],
-    "@babel/plugin-proposal-export-default-from",
-    "@babel/plugin-proposal-export-namespace-from",
-    ["@babel/plugin-proposal-object-rest-spread", { useBuiltIns: true }],
-    "@babel/plugin-proposal-optional-chaining",
-    "@babel/plugin-proposal-nullish-coalescing-operator",
-    "@babel/plugin-syntax-async-generators",
-    "@babel/plugin-syntax-dynamic-import",
-    ["@babel/plugin-transform-destructuring", { useBuiltIns: true }],
-    ["@babel/plugin-transform-runtime", { helpers: false, regenerator: true }],
-  ].filter(Boolean),
+  ]
 };
